c3c07b3f41ab5f252184d0a6c467b03d4c36ea1e
HarshitSomal <hkaurece@gmail.com>
2016-02-19 22:04:12 -0800

    Initial commit

---
+++ app/src/main/java/harshit/spinnercode/MainActivity.java
@@ -0,0 +1,44 @@
+package harshit.spinnercode;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Spinner element
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        String[]items={"ITEM1","ITEM2","ITEM3"};
+        // Creating adapter for spinner
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, items);
+
+        // Spinner click listener
+        spinner.setOnItemSelectedListener(this);
+// attaching data adapter to spinner
+        spinner.setAdapter(adapter);
+    }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        // On selecting a spinner item
+        String item = parent.getItemAtPosition(position).toString();
+
+        // Showing selected spinner item
+        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
+
